dea74420d40d750020c76be05d7c8d5da694352e
Ripan Roy <88824889+Ripan-Roy@users.noreply.github.com>
2022-05-03 23:05:52 +0530

    Create RemoveDuplicatesFromSortedArray.cpp

---
+++ RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,10 @@
+//RemoveDuplicatesFromSortedArray
+T.C. - O(n)
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        return nums.size();
+    }
+};
